eadf305de309ff5f8e87343b90d2c057c044e430
Diana Kalstein <dianakalstein@gmail.com>
2026-06-13 19:12:27 +0200

    feat(registry): implement risk profiles and versioned assessments

---
+++ src/api/process-assessment/controllers/process-assessment.ts
@@ -0,0 +1,7 @@
+/**
+ * process-assessment controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::process-assessment.process-assessment');
---
+++ src/api/risk-profile/services/risk-profile.ts
@@ -0,0 +1,7 @@
+/**
+ * risk-profile service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::risk-profile.risk-profile');
